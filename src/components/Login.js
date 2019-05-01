1debdc56d782e5e7947387b3d1ef9aa96e0a8a11
Aiden Buis <aidenbuis@gmail.com>
2019-05-01 15:54:29 +0200

    Add shake message to login

--- src/components/Login.js
+++ src/components/Login.js
@@ -60,6 +60,10 @@ class Login extends Component {
 			errorEl.classList.innerHTML = err;
 			errorEl.classList.remove('hidden');
 			errorEl.classList.add('flex');
+			errorEl.classList.add('shake');
+			setTimeout(function(){ 
+				errorEl.classList.remove('shake');
+			}, 820);
 
 		});
 
@@ -71,6 +75,13 @@ class Login extends Component {
 		errorEl.classList.remove('flex');
   }
 
+	handleChange(e){
+		this.setState(
+			{
+				[e.target.name]: e.target.value
+			}
+		)
+	}
 
 	render() {
 		return (
@@ -84,13 +95,13 @@ class Login extends Component {
 							<label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="identifier">
 								E-mailadres
 							</label>
-							<input className="shadow appearance-none border rounded w-full py-3 leading-loose px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" name="identifier" id="identifier" type="text" placeholder="bijv. f.vermeulen@pzh.nl" onChange={this.handleChange}/>
+							<input required className="shadow appearance-none border rounded w-full py-3 leading-loose px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline" name="identifier" id="identifier" type="text" placeholder="bijv. f.vermeulen@pzh.nl" onChange={this.handleChange}/>
 						</div>
 						<div className="mb-4 mt-6">
 							<label className="block text-grey-darker text-sm font-bold mb-2" htmlFor="password">
 								Wachtwoord
 							</label>
-							<input className="shadow appearance-none border rounded w-full py-4 pb-3 leading-loose px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline" name="password" id="password" type="password" placeholder="******************" onChange={this.handleChange}/>
+							<input required className="shadow appearance-none border rounded w-full py-4 pb-3 leading-loose px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline" name="password" id="password" type="password" placeholder="******************" onChange={this.handleChange}/>
 						</div>
 						<div className="">
 							<button className="mbg-color hover:bg-blue-dark text-white inline-block py-2 px-8 rounded focus:outline-none focus:shadow-outline" type="button" type="submit">
@@ -99,7 +110,7 @@ class Login extends Component {
 						</div>
 					</form>
 					<div id="error-message" className="container items-center justify-center hidden">
-						<div className="bg-red-lightest border border-red-light pr-10 text-red-dark px-4 py-3 rounded relative inline-block" role="alert">
+						<div className="bg-red-lightest w-full border border-red-light pr-10 text-red-dark px-4 py-3 rounded relative inline-block" role="alert">
 						  <span className="block sm:inline">Wrong username or password.</span>
 						  <span className="absolute pin-t pin-b pin-r px-4 py-3" onClick={this.handleErrorMessage}>
 						    <svg className="fill-current h-6 w-6 text-red" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
@@ -113,15 +124,6 @@ class Login extends Component {
 			</div>
 			
 		);
-	}
-
-	handleChange(e){
-
-		this.setState(
-			{
-				[e.target.name]: e.target.value
-			}
-		)
 	}
 
 }
