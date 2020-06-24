a2ad0d919ea29b2537fc9a0df639c63872d2ad84
Aiden Buis <hello@aidenbuis.com>
2020-06-24 11:38:32 +0200

    ✨ Add utility hooks for outside click & escape key

---
+++ src/utils/useClickOutsideContainer.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react'
+
+function useClickOutsideContainer(ref, callback) {
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (ref.current && !ref.current.contains(event.target)) {
+                callback()
+            }
+        }
+
+        // Bind the event listener
+        document.addEventListener('mousedown', handleClickOutside)
+        return () => {
+            // Unbind the event listener on clean up
+            document.removeEventListener('mousedown', handleClickOutside)
+        }
+    }, [ref])
+}
+
+export default useClickOutsideContainer
