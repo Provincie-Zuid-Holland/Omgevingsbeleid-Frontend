19f4999f8f3aa84ce05d8d1fcce911aa8a75770a
Aiden Buis <hello@aidenbuis.com>
2020-10-20 15:51:33 +0200

    ✅ Add test values

--- src/constants/maatregelen.js
+++ src/constants/maatregelen.js
@@ -1,3 +1,5 @@
+import { currentDate, futureDate, currentDateFormatted } from './testValues'
+
 export const TITEL_ENKELVOUD = 'Maatregel'
 export const TITEL_MEERVOUD = 'Maatregelen'
 export const API_ENDPOINT = 'maatregelen'
@@ -10,40 +12,56 @@ export const CRUD_PROPERTIES = {
         initValue: null,
         required: true,
         requiredMessage: 'Vul een titel in',
+        testValue: `Test beleidsregel ${currentDateFormatted}`,
+        type: 'text input',
     },
     Gebied: {
         initValue: null,
         required: false,
         requiredMessage: '',
+        testValue: null,
+        type: 'select gebied',
     },
     Toelichting: {
         initValue: '',
         required: false,
         requiredMessage: '',
+        testValue: `Toelichting`,
+        type: 'text input',
     },
     Gebied_Duiding: {
-        initValue: null,
+        initValue: `Indicatief`,
         required: false,
         requiredMessage: '',
+        testValue: `Indicatief`,
+        type: 'radio input',
     },
     Weblink: {
         initValue: null,
         required: false,
         requiredMessage: '',
+        testValue: `Toelichting`,
+        type: 'text input',
     },
     Status: {
         initValue: 'Ontwerp GS Concept',
         required: false,
         requiredMessage: '',
+        testValue: null,
+        type: 'hidden', // Property is automatically generated and not displayed in the UI
     },
     Begin_Geldigheid: {
         initValue: null,
         required: false,
         requiredMessage: 'Vul een inwerkingtreding datum in',
+        testValue: currentDate,
+        type: 'date input',
     },
     Eind_Geldigheid: {
         initValue: null,
         required: false,
         requiredMessage: 'Vul een uitwerkingtreding datum in',
+        testValue: futureDate,
+        type: 'date input',
     },
 }
