179e8e0ac23486fbe5695a7db9c3d6893cfd299a
Aiden Buis <hello@aidenbuis.com>
2020-10-20 15:46:30 +0200

    ✨ Update Sentry

--- src/App/App.js
+++ src/App/App.js
@@ -44,7 +44,10 @@ import * as Sentry from '@sentry/browser'
 
 const AuthRoutes = lazy(() => import('./AuthRoutes'))
 
-if (process.env.REACT_APP_SENTRY_DSN) {
+if (
+    process.env.REACT_APP_SENTRY_DSN &&
+    process.env.NODE_ENV !== 'development'
+) {
     Sentry.init({
         environment: process.env.NODE_ENV,
         release: process.env.REACT_APP_RELEASE_VERSION,
