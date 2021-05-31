333e2f9688df782cfb21adb836bded5b735d237a
Aiden Buis <hello@aidenbuis.com>
2021-05-31 15:00:46 +0200

    🐛 Fix textual relation #AB6041

--- src/components/RelatiesKoppelingenTekstueel/RelatiesKoppelingenTekstueel.js
+++ src/components/RelatiesKoppelingenTekstueel/RelatiesKoppelingenTekstueel.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link, useLocation } from 'react-router-dom'
 
-const getObjectFromRelation = (object) =>
-    object.hasOwnProperty('Van_Beleidskeuze')
-        ? object.Van_Beleidskeuze
-        : object.hasOwnProperty('Naar_Beleidskeuze')
-        ? object.Naar_Beleidskeuze
-        : null
+const getObjectFromRelation = (relation) => {
+    return relation.hasOwnProperty('Van_Beleidskeuze')
+        ? relation.Van_Beleidskeuze
+        : relation.hasOwnProperty('Naar_Beleidskeuze')
+        ? relation.Naar_Beleidskeuze
+        : relation
+}
 
 function RelatiesKoppelingenTekstueel({
     beleidsObject,
