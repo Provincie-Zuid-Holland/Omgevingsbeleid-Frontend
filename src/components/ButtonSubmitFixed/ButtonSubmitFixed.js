d0c1ff0c89a57da4adf037ccf2a5c31fb1117548
Aiden Buis <hello@aidenbuis.com>
2021-06-16 13:25:40 +0200

    🐛 Prevent submit on Enter

--- src/components/ButtonSubmitFixed/ButtonSubmitFixed.js
+++ src/components/ButtonSubmitFixed/ButtonSubmitFixed.js
@@ -4,14 +4,26 @@ import React from 'react'
  *
  * @component
  */
-function ButtonSubmitFixed() {
+function ButtonSubmitFixed({ submit }) {
     return (
         <div className="fixed bottom-0 right-0 z-10 px-6">
             <div className="inline-block px-4 py-4 bg-white rounded-t shadow">
                 <button
                     id="form-submit"
                     className="px-4 py-2 text-sm font-bold leading-tight text-white rounded cursor-pointer bg-pzh-blue hover:underline"
-                    type="submit"
+                    type="button"
+                    onClick={(e) => {
+                        if (!submit) return
+                        submit(e)
+                    }}
+                    onKeyPress={(e) => {
+                        if (!submit) {
+                            return
+                        }
+                        if (e.key === 'Enter') {
+                            submit(e)
+                        }
+                    }}
                 >
                     Opslaan
                 </button>
