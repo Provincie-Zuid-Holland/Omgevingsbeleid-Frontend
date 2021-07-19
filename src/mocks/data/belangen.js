b08075ca862f2e242e5969d28b843208c99afc60
Aiden Buis <hello@aidenbuis.com>
2021-07-19 19:06:15 +0200

    🤡 Add Mock Data

---
+++ src/mocks/data/belangen.js
@@ -0,0 +1,118 @@
+const belangen = [
+    {
+        ID: 140,
+        UUID: "EEF074A3-29A5-45B9-81A0-E51D5368107F",
+        Begin_Geldigheid: "1753-01-01T00:00:00Z",
+        Eind_Geldigheid: "9999-12-31T23:59:59Z",
+        Created_By: {
+            UUID: "359A9254-598E-4BFE-BCEC-AA521E95881A",
+            Gebruikersnaam: "Aiden Buis",
+            Rol: "Beheerder",
+            Status: "Actief",
+        },
+        Created_Date: "2021-03-22T19:17:59.933000Z",
+        Modified_By: {
+            UUID: "359A9254-598E-4BFE-BCEC-AA521E95881A",
+            Gebruikersnaam: "Aiden Buis",
+            Rol: "Beheerder",
+            Status: "Actief",
+        },
+        Modified_Date: "2021-03-22T19:17:59.933000Z",
+        Titel: "##111 Taak",
+        Omschrijving: null,
+        Weblink: null,
+        Type: "Wettelijke Taak & Bevoegdheid",
+        Ref_Beleidskeuzes: [
+            {
+                ID: 732,
+                UUID: "B8A79FDE-ED2E-4AEC-B8E7-759C2E4A6B3A",
+                Titel:
+                    "Een uitstekend ecosysteem voor innovatie en ondernemerschap bevorderen",
+            },
+        ],
+    },
+    {
+        ID: 139,
+        UUID: "DBCD1E8D-3F87-48F3-845B-1F44A89EE5EE",
+        Begin_Geldigheid: "2021-02-03T00:00:00Z",
+        Eind_Geldigheid: "2021-08-03T00:00:00Z",
+        Created_By: {
+            UUID: "359A9254-598E-4BFE-BCEC-AA521E95881A",
+            Gebruikersnaam: "Aiden Buis",
+            Rol: "Beheerder",
+            Status: "Actief",
+        },
+        Created_Date: "2021-02-03T14:02:31.140000Z",
+        Modified_By: {
+            UUID: "359A9254-598E-4BFE-BCEC-AA521E95881A",
+            Gebruikersnaam: "Aiden Buis",
+            Rol: "Beheerder",
+            Status: "Actief",
+        },
+        Modified_Date: "2021-02-03T14:02:31.140000Z",
+        Titel: "Test ambitie woensdag 3 februari 2021",
+        Omschrijving: "Omschrijving",
+        Weblink: "Weblink",
+        Type: "Nationaal Belang",
+        Ref_Beleidskeuzes: [
+            {
+                ID: 732,
+                UUID: "B8A79FDE-ED2E-4AEC-B8E7-759C2E4A6B3A",
+                Titel:
+                    "Een uitstekend ecosysteem voor innovatie en ondernemerschap bevorderen",
+            },
+        ],
+    },
+    {
+        ID: 49,
+        UUID: "3346879C-E3C0-42D5-A58B-2573CA90F280",
+        Begin_Geldigheid: "1753-01-01T00:00:00Z",
+        Eind_Geldigheid: "9999-12-31T23:59:59Z",
+        Created_By: {
+            UUID: "9D4F11BE-A502-4DCE-A6FA-24FB827FD6B7",
+            Gebruikersnaam: "Erik Verhaar",
+            Rol: "Beheerder",
+            Status: "Actief",
+        },
+        Created_Date: "2020-10-30T08:03:32.317000Z",
+        Modified_By: {
+            UUID: "9D4F11BE-A502-4DCE-A6FA-24FB827FD6B7",
+            Gebruikersnaam: "Erik Verhaar",
+            Rol: "Beheerder",
+            Status: "Actief",
+        },
+        Modified_Date: "2020-10-30T08:03:32.317000Z",
+        Titel: "21. Ontwikkelen van een duurzame visserij",
+        Omschrijving: null,
+        Weblink: "Nationaal Belang",
+        Type: null,
+        Ref_Beleidskeuzes: [],
+    },
+    {
+        ID: 48,
+        UUID: "50F78F63-002A-4129-AA76-69B53A6EF659",
+        Begin_Geldigheid: "1753-01-01T00:00:00Z",
+        Eind_Geldigheid: "9999-12-31T23:59:59Z",
+        Created_By: {
+            UUID: "9D4F11BE-A502-4DCE-A6FA-24FB827FD6B7",
+            Gebruikersnaam: "Erik Verhaar",
+            Rol: "Beheerder",
+            Status: "Actief",
+        },
+        Created_Date: "2020-10-30T08:03:21.897000Z",
+        Modified_By: {
+            UUID: "9D4F11BE-A502-4DCE-A6FA-24FB827FD6B7",
+            Gebruikersnaam: "Erik Verhaar",
+            Rol: "Beheerder",
+            Status: "Actief",
+        },
+        Modified_Date: "2020-10-30T08:03:21.897000Z",
+        Titel: "20. Verbeteren en beschermen van natuur en biodiversiteit",
+        Omschrijving: null,
+        Weblink: "Nationaal Belang",
+        Type: null,
+        Ref_Beleidskeuzes: [],
+    },
+]
+
+export { belangen }
