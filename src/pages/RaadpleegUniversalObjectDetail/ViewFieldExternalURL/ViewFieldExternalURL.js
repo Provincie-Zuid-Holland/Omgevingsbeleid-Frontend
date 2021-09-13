dcdf0001d09f2fb6689a4f7ec61a7df1f167698f
Aiden Buis <hello@aidenbuis.com>
2021-09-13 13:54:58 +0200

    🐛 #AB5823 - Always open in new tab

--- src/pages/RaadpleegUniversalObjectDetail/ViewFieldExternalURL/ViewFieldExternalURL.js
+++ src/pages/RaadpleegUniversalObjectDetail/ViewFieldExternalURL/ViewFieldExternalURL.js
@@ -5,9 +5,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 const ViewFieldExternalURL = ({ externalURL }) => {
     if (!externalURL) return null
 
+    // https://stackoverflow.com/questions/43803778/href-without-https-prefix
+    const getClickableLink = (link) => {
+        return link.startsWith("http://") || link.startsWith("https://")
+            ? link
+            : `//${link}`
+    }
+
     return (
         <a
-            href={externalURL}
+            href={getClickableLink(externalURL)}
             target="_blank"
             rel="noreferrer"
             className="mt-5 text-pzh-blue hover:text-pzh-blue-dark"
