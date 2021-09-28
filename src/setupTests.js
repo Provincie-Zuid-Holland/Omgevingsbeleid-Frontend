add2c366987957fc617c28e18668882c71cf3e0e
Aiden Buis <hoi@aidenbuis.com>
2021-09-28 15:42:40 +0200

    Log baseurl

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,9 +1,11 @@
 // src/setupTests.js
 import { server } from "./mocks/server.js"
+import { baseURL } from "./API/axios"
 
 // Establish API mocking before all tests.
 beforeAll(() => {
     server.listen()
+    console.log(baseURL)
 })
 
 // Reset any request handlers that we may add during the tests,
