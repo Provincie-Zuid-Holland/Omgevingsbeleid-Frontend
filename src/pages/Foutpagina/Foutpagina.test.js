7d7af55d6028940b9568340d3eb68bc1bbe6d54c
JasonkAEC <jason.kalmijn@effectivechanges.com>
2021-11-23 12:53:47 +0100

    Altered foutpagina test file

--- src/pages/Foutpagina/Foutpagina.test.js
+++ src/pages/Foutpagina/Foutpagina.test.js
@@ -1,16 +1,10 @@
 import { render } from "@testing-library/react"
 import React from "react"
 import Foutpagina from "./Foutpagina"
-import { Helmet } from "react-helmet"
-import { MemoryRouter } from "react-router-dom"
 
 describe("Foutpagina", () => {
     it("should render", () => {
-        const component = render(
-            <MemoryRouter>
-                <Foutpagina />
-            </MemoryRouter>
-        )
+        const component = render(<Foutpagina />)
 
         expect(component).toBeTruthy()
     })
