1bd1c0c4d5213e50aaf8cef1495893692e84fae6
Aiden Buis <hello@aidenbuis.com>
2021-12-21 13:16:50 +0100

    ✨ Fixed on scroll

--- src/pages/RaadpleegZoekResultatenOverzicht/SearchFilterSection/SearchFilterSection.js
+++ src/pages/RaadpleegZoekResultatenOverzicht/SearchFilterSection/SearchFilterSection.js
@@ -1,5 +1,6 @@
 import React from "react"
 
+import { useWindowSize } from "./../../../utils/useWindowSize"
 import LoaderCard from "./../../../components/LoaderCard"
 
 import FilterItem from "./../FilterItem"
@@ -11,6 +12,44 @@ const SearchFilterSection = ({
     filters,
     handleFilter,
 }) => {
+    const container = React.useRef(null)
+    const windowSize = useWindowSize()
+    const [containerStyle, setContainerStyle] = React.useState({})
+
+    React.useEffect(() => {
+        const handleScroll = () => {
+            const windowScrollTop =
+                window.pageYOffset || document.documentElement.scrollTop
+
+            if (windowScrollTop > container.current.offsetTop) {
+                const { offsetWidth, offsetHeight, offsetTop, offsetLeft } =
+                    container.current
+
+                const fixedStyle = {
+                    width: offsetWidth,
+                    height: offsetHeight,
+                    top: offsetTop,
+                    left: offsetLeft,
+                    position: "fixed",
+                }
+
+                setContainerStyle(fixedStyle)
+            } else if (
+                windowSize.width < 768 ||
+                windowScrollTop < container.current.offsetTop
+            ) {
+                const relativeStyle = {
+                    position: "relative",
+                }
+
+                setContainerStyle(relativeStyle)
+            }
+        }
+
+        window.addEventListener("scroll", handleScroll)
+        return () => window.removeEventListener("scroll", handleScroll)
+    }, [windowSize])
+
     const searchFilterCategories = {
         Omgevingsvisie: ["ambities", "beleidsdoelen", "beleidskeuzes"],
         Omgevingsprogramma: ["maatregelen"],
@@ -26,44 +65,53 @@ const SearchFilterSection = ({
     }
 
     return (
-        <div className="col-span-6 mt-0 md:col-span-2 md:mt-4">
-            {searchFiltersOnly === null &&
-            onPageFilters?.filterArray?.length > 0
-                ? Object.keys(searchFilterCategories)
-                      .filter((category) =>
-                          filters.some((filter) =>
-                              searchFilterCategories[category].includes(filter)
+        <div ref={container} className="col-span-6 pt-0 md:col-span-2 md:pt-4">
+            <div style={containerStyle}>
+                {searchFiltersOnly === null &&
+                onPageFilters?.filterArray?.length > 0
+                    ? Object.keys(searchFilterCategories)
+                          .filter((category) =>
+                              filters.some((filter) =>
+                                  searchFilterCategories[category].includes(
+                                      filter
+                                  )
+                              )
                           )
-                      )
-                      .map((category) => (
-                          <div>
-                              <span className="mt-2 font-bold text-pzh-blue">
-                                  {category}
-                              </span>
-                              <ul id="filter-search-results" className="mb-4">
-                                  {filters
-                                      .filter((filterCategory) =>
-                                          searchFilterCategories[
-                                              category
-                                          ].includes(filterCategory)
-                                      )
-                                      .map((filter) => (
-                                          <FilterItem
-                                              key={filter}
-                                              count={
-                                                  onPageFilters[filter].count
-                                              }
-                                              handleFilter={handleFilter}
-                                              checked={
-                                                  !onPageFilters[filter].checked
-                                              }
-                                              item={filter}
-                                          />
-                                      ))}
-                              </ul>
-                          </div>
-                      ))
-                : null}
+                          .map((category) => (
+                              <div>
+                                  <span className="mt-2 font-bold text-pzh-blue">
+                                      {category}
+                                  </span>
+                                  <ul
+                                      id="filter-search-results"
+                                      className="mb-4"
+                                  >
+                                      {filters
+                                          .filter((filterCategory) =>
+                                              searchFilterCategories[
+                                                  category
+                                              ].includes(filterCategory)
+                                          )
+                                          .map((filter) => (
+                                              <FilterItem
+                                                  key={filter}
+                                                  count={
+                                                      onPageFilters[filter]
+                                                          .count
+                                                  }
+                                                  handleFilter={handleFilter}
+                                                  checked={
+                                                      !onPageFilters[filter]
+                                                          .checked
+                                                  }
+                                                  item={filter}
+                                              />
+                                          ))}
+                                  </ul>
+                              </div>
+                          ))
+                    : null}
+            </div>
         </div>
     )
 }
