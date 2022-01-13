180c0fa176e5d0cb77dc04ab3ca4b9d8e122ec44
Stef Winterswijk <stef_winterswijk@hotmail.com>
2022-01-13 14:10:54 +0100

    Change case of api folder

---
+++ src/api/instance.ts
@@ -0,0 +1,60 @@
+import axios, { AxiosRequestConfig } from 'axios'
+
+export type Environment = 'dev' | 'test' | 'acc' | 'prod'
+
+const api_version = process.env.REACT_APP_API_VERSION
+const environment = process.env.REACT_APP_API_ENV as Environment
+
+const getAccessToken = () =>
+    localStorage.getItem(process.env.REACT_APP_KEY_API_ACCESS_TOKEN || '')
+
+const apiURLS = {
+    dev: `https://api-obzh-dev.azurewebsites.net/${api_version}`,
+    test: `https://api-obzh-test.azurewebsites.net/${api_version}`,
+    acc: `https://api-obzh-acc.azurewebsites.net/${api_version}`,
+    prod: `https://api-obzh.azurewebsites.net/${api_version}`,
+}
+
+const instance = axios.create({
+    baseURL: apiURLS[environment],
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
+
+instance.interceptors.request.use(function (config) {
+    config.headers &&
+        (config.headers.Authorization = `Bearer ${getAccessToken()}`)
+    return config
+})
+
+instance.interceptors.response.use(
+    function (response) {
+        return response
+    },
+    function (error) {
+        const allowedUrls = ['password-reset']
+        if (
+            error?.response?.status === 401 &&
+            !allowedUrls.includes(error?.response?.config?.url)
+        ) {
+            window.dispatchEvent(
+                new CustomEvent('authEvent', {
+                    detail: { message: 'Authenticated sessie is afgelopen' },
+                })
+            )
+        } else {
+            return Promise.reject(error)
+        }
+    }
+)
+
+const baseURL = instance.defaults.baseURL
+
+export const customInstance = <T>(config: AxiosRequestConfig): Promise<T> => {
+    const promise = instance({ ...config }).then(({ data }) => data)
+
+    return promise
+}
+
+export { environment, baseURL }
