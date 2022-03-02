829c2a20a7531390c6b30ddca2d3395844a03dcd
Stef Winterswijk <stef_winterswijk@hotmail.com>
2022-03-02 13:33:17 +0100

    Add comment

--- src/a11y.ts
+++ src/a11y.ts
@@ -1,3 +1,6 @@
+/* istanbul ignore file */
+// If environment is not Production and not running Jest
+
 export default function useAxe() {
     const React = require('react')
     const ReactDOM = require('react-dom')
