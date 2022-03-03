61d81542b78136bc301106bc4ff0a649bdff121d
Stef Winterswijk <stef_winterswijk@hotmail.com>
2022-03-03 14:15:08 +0100

    Convert to tsx

--- src/components/Truncate/Truncate.tsx
+++ src/components/Truncate/Truncate.tsx
@@ -1,19 +1,21 @@
-import PropTypes from 'prop-types'
 import { Component, createRef } from 'react'
 import xss from 'xss'
 
 const isServer = () => typeof window === 'undefined'
 
-class Truncate extends Component {
-    static propTypes = {
-        ellipsis: PropTypes.string,
-        debounce: PropTypes.number,
-        responsive: PropTypes.bool,
-        lines: PropTypes.number,
-        portrait: PropTypes.number,
-        breakWord: PropTypes.bool,
+interface TruncateProps {
+    ellipsis?: string
+    debounce?: number
+    responsive?: boolean
+    lines?: number
+    portrait?: number
+    breakWord?: boolean
+    dangerouslySetInnerHTML: {
+        __html: string
     }
+}
 
+class Truncate extends Component<TruncateProps> {
     static defaultProps = {
         ellipsis: '…',
         debounce: 100,
@@ -23,13 +25,19 @@ class Truncate extends Component {
         breakWord: true,
     }
 
-    paragraph = createRef()
+    paragraph = createRef<HTMLSpanElement>()
+
+    cached: string | null | undefined = null
+    conf: any
+    prop: any
+    lines: any
+    _listener: any
 
     render() {
         // pass any additional props to the paragraph element
         const passedProps = { ...this.props }
-        for (const key of Object.keys(Truncate.propTypes)) {
-            delete passedProps[key]
+        for (const key of Object.keys(Truncate.defaultProps)) {
+            delete passedProps[key as keyof typeof Truncate.defaultProps]
         }
 
         if (this.props.children) {
@@ -53,12 +61,12 @@ class Truncate extends Component {
         )
     }
 
-    componentDidUpdate(prevProps) {
+    componentDidUpdate(prevProps: TruncateProps) {
         if (
             prevProps.dangerouslySetInnerHTML !==
             this.props.dangerouslySetInnerHTML
         ) {
-            this.cached = this.paragraph.current.innerHTML
+            this.cached = this.paragraph.current?.innerHTML
             this.add()
         }
     }
@@ -79,12 +87,14 @@ class Truncate extends Component {
             }
 
             if (this.props.responsive) {
-                this.cached = this.paragraph.current.innerHTML
-                let debounce
-                const listener = function () {
+                this.cached = this.paragraph.current?.innerHTML
+                let debounce: any
+
+                const listener = function (this: any) {
                     clearTimeout(debounce)
                     debounce = setTimeout(
                         function () {
+                            // @ts-ignore
                             this.add()
                         }.bind(this),
                         this.props.debounce
@@ -105,27 +115,29 @@ class Truncate extends Component {
         }
     }
 
-    cached = null
-
-    createProp(element) {
+    createProp(element: HTMLSpanElement) {
         this.prop = {
             get height() {
                 const viewportOffset = element.getBoundingClientRect()
 
-                return parseInt(viewportOffset.bottom - viewportOffset.top, 10)
+                return parseInt(
+                    `${viewportOffset.bottom - viewportOffset.top}`,
+                    10
+                )
             },
             get lineheight() {
                 let lineh =
                     getComputedStyle(element).getPropertyValue('line-height')
                 if (String('normal|initial|inherit').indexOf(lineh) > -1) {
                     //very specific case
-                    lineh =
+                    lineh = `${
                         parseInt(
                             getComputedStyle(element).getPropertyValue(
                                 'font-size'
                             ),
                             10
                         ) + 2
+                    }`
                 }
                 return parseInt(lineh, 10)
             },
@@ -133,9 +145,11 @@ class Truncate extends Component {
     }
 
     add() {
+        if (!this.paragraph.current) return
+
         if (this.props.responsive) {
             if (this.paragraph.current.innerHTML !== this.cached) {
-                this.paragraph.current.innerHTML = this.cached
+                this.paragraph.current.innerHTML = this.cached || ''
             }
         }
 
@@ -157,12 +171,14 @@ class Truncate extends Component {
         }
     }
 
-    breakWord(str, str2, fix) {
+    breakWord(str: string, str2?: string | null, fix?: boolean) {
         const words = str.split(' ')
         words.pop()
+
         if (fix) {
             words.pop()
         }
+
         if (!str2) {
             if (words[words.length - 1]) {
                 words[words.length - 1] = words[words.length - 1]
@@ -170,6 +186,7 @@ class Truncate extends Component {
                     .replace(/(\.$)/g, '')
             }
             words.push(this.conf.ellipsis)
+
             return words.join(' ')
         } else {
             if (words[words.length - 1]) {
@@ -177,6 +194,7 @@ class Truncate extends Component {
                     .replace(/(,$)/g, '')
                     .replace(/(\.$)/g, '')
                 words.push(this.conf.ellipsis)
+
                 return [words.join(' '), str2]
             } else if (!words[words.length - 1] && str2) {
                 const st =
@@ -184,29 +202,36 @@ class Truncate extends Component {
                     str2.trim().replace(/(,$)/g, '').replace(/(\.$)/g, '') +
                     ' '
                 words.push(this.conf.ellipsis)
+
                 return [words.join(' '), st]
             }
         }
+
+        return null
     }
 
-    simpleText(element) {
+    simpleText(element: any) {
         let childText = element.childNodes[0].nodeValue
+
+        if (!childText) return
+
         while (this.prop.height > this.prop.lineheight * this.lines.current) {
             element.childNodes[0].nodeValue = childText.slice(0, -1)
             childText = element.childNodes[0].nodeValue
         }
+
         if (this.props.breakWord) {
             element.childNodes[0].nodeValue =
-                childText.slice(0, -this.props.ellipsis.length) +
+                childText.slice(0, -this.props.ellipsis!.length) +
                 this.props.ellipsis
             if (this.isNotCorrect()) {
                 //edge case
                 element.childNodes[0].nodeValue =
                     ' ' +
                     element.childNodes[0].nodeValue
-                        .slice(0, -(this.props.ellipsis.length + 1))
+                        .slice(0, -(this.props.ellipsis!.length + 1))
                         .trim()
-                        .slice(0, -this.props.ellipsis.length) +
+                        .slice(0, -this.props.ellipsis!.length) +
                     this.props.ellipsis
             }
         } else {
@@ -228,12 +253,14 @@ class Truncate extends Component {
         return this.prop.height > this.prop.lineheight * this.lines.current
     }
 
-    processBreak(dOne, dTwo, fix) {
-        const r = this.breakWord(
-            dOne.innerHTML || dOne.nodeValue,
-            dTwo.innerHTML || dTwo.nodeValue,
-            fix
-        )
+    processBreak(dOne: HTMLSpanElement, dTwo: HTMLSpanElement, fix?: boolean) {
+        const r =
+            this.breakWord(
+                dOne.innerHTML || dOne.nodeValue || '',
+                dTwo.innerHTML || dTwo.nodeValue,
+                fix
+            ) || []
+
         if (dOne.innerHTML) {
             dOne.innerHTML = r[0]
         } else {
@@ -246,12 +273,13 @@ class Truncate extends Component {
         }
     }
 
-    handleChilds(e) {
-        const domChilds = e.childNodes
+    handleChilds(e: HTMLSpanElement) {
+        const domChilds = e.childNodes as NodeListOf<HTMLElement>
         let childText
 
         for (let i = domChilds.length - 1; i >= 0; i--) {
             let displayOrigin
+
             if (domChilds[i].nodeType === 3) {
                 displayOrigin = domChilds[i].nodeValue
                 domChilds[i].nodeValue = ''
@@ -262,35 +290,43 @@ class Truncate extends Component {
                 domChilds[i].style.display = 'none'
             }
 
-            if (this.prop.height <= this.prop.lineheight * this.lines.current) {
+            if (
+                this.prop.height <= this.prop.lineheight * this.lines.current &&
+                displayOrigin
+            ) {
                 if (domChilds[i].nodeType === 3) {
                     domChilds[i].nodeValue = displayOrigin
                     childText = domChilds[i].nodeValue
+
                     while (
                         this.prop.height >
-                        this.prop.lineheight * this.lines.current
+                            this.prop.lineheight * this.lines.current &&
+                        childText
                     ) {
                         domChilds[i].nodeValue = childText.slice(0, -1)
                         childText = domChilds[i].nodeValue
                     }
 
-                    if (this.props.breakWord) {
+                    if (this.props.breakWord && childText) {
                         domChilds[i].nodeValue =
-                            childText.slice(0, -this.props.ellipsis.length) +
+                            childText.slice(0, -this.props.ellipsis!.length) +
                             this.props.ellipsis
                         if (this.isNotCorrect()) {
                             //edge case
                             domChilds[i].nodeValue =
                                 ' ' +
                                 domChilds[i].nodeValue
-                                    .slice(0, -this.props.ellipsis.length)
+                                    ?.slice(0, -this.props.ellipsis!.length)
                                     .trim()
-                                    .slice(0, -this.props.ellipsis.length)
-                            if (domChilds[i].nodeValue.length > 1) {
+                                    .slice(0, -this.props.ellipsis!.length)
+                            if (
+                                domChilds[i].nodeValue?.length &&
+                                domChilds[i].nodeValue!.length > 1
+                            ) {
                                 domChilds[i].nodeValue =
-                                    domChilds[i].nodeValue.slice(
+                                    domChilds[i].nodeValue!.slice(
                                         0,
-                                        -this.props.ellipsis.length
+                                        -this.props.ellipsis!.length
                                     ) + this.props.ellipsis
                             } else {
                                 continue
@@ -329,21 +365,21 @@ class Truncate extends Component {
                     }
                     if (this.props.breakWord) {
                         domChilds[i].innerHTML =
-                            childText.slice(0, -this.props.ellipsis.length) +
+                            childText.slice(0, -this.props.ellipsis!.length) +
                             this.props.ellipsis
                         if (this.isNotCorrect()) {
                             //edge case
                             domChilds[i].innerHTML =
                                 ' ' +
                                 domChilds[i].innerHTML
-                                    .slice(0, -this.props.ellipsis.length)
+                                    .slice(0, -this.props.ellipsis!.length)
                                     .trim()
-                                    .slice(0, -this.props.ellipsis.length)
+                                    .slice(0, -this.props.ellipsis!.length)
                             if (domChilds[i].innerHTML.length > 1) {
                                 domChilds[i].innerHTML =
                                     domChilds[i].innerHTML.slice(
                                         0,
-                                        -this.props.ellipsis.length
+                                        -this.props.ellipsis!.length
                                     ) + this.props.ellipsis
                             } else {
                                 continue
