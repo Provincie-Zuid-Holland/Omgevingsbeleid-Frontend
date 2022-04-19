2f307e7c1766fd564bf1c5218395348eda91a32f
Aiden Buis <hello@aidenbuis.com>
2022-04-19 14:55:14 +0200

    ♻️ Remove old components

--- src/pages/MuteerUniversalObjectCRUD/FormFieldContainers/FormFieldContainerBeleidsprestaties/FormFieldContainerBeleidsprestaties.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react'
-
-import FormFieldContainerBeleidsprestaties from './FormFieldContainerBeleidsprestaties'
-
-describe('FormFieldContainerBeleidsprestaties', () => {
-    const defaultProps = {}
-
-    it('should render', () => {})
-})
