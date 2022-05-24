2e3571dc875c35e5a27303b5299e190647b242ed
Aiden Buis <hello@aidenbuis.com>
2022-05-24 11:50:31 +0200

    ♻️ Remove unused pages and components
    From refactoring the Detail and Overview pages

--- src/components/Container/index.ts
+++ src/components/Container/index.ts
@@ -1,13 +1,6 @@
 import Container from './Container'
-import ContainerDetailMain from './ContainerDetailMain'
 import ContainerFormSection from './ContainerFormSection'
 import ContainerMain from './ContainerMain'
 import ContainerMapSearch from './ContainerMapSearch'
 
-export {
-    Container,
-    ContainerDetailMain,
-    ContainerFormSection,
-    ContainerMain,
-    ContainerMapSearch,
-}
+export { Container, ContainerFormSection, ContainerMain, ContainerMapSearch }
