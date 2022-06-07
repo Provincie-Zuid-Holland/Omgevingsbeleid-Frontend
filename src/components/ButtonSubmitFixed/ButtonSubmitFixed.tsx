276dd9548b9a735163913825ea10fe0004557904
Aiden Buis <hello@aidenbuis.com>
2022-06-07 11:05:00 +0200

    ✨ Add disabled property and replace with PZH UI

--- src/components/ButtonSubmitFixed/ButtonSubmitFixed.tsx
+++ src/components/ButtonSubmitFixed/ButtonSubmitFixed.tsx
@@ -1,28 +1,32 @@
+import { Button } from '@pzh-ui/components'
+import React from 'react'
+
 /**
  * Displays a fixed placed submit button.
  */
 interface ButtonSubmitFixedProps {
+    disabled?: boolean
     submit?: any
 }
 
-function ButtonSubmitFixed({ submit }: ButtonSubmitFixedProps) {
+function ButtonSubmitFixed({ disabled, submit }: ButtonSubmitFixedProps) {
     if (!submit) return null
 
     return (
         <div className="fixed bottom-0 right-0 z-10 px-6">
             <div className="inline-block px-4 py-4 bg-white rounded-t shadow">
-                <button
+                <Button
+                    disabled={disabled}
                     id="form-submit"
-                    className="px-4 py-2 text-sm font-bold leading-tight text-white rounded cursor-pointer bg-pzh-blue hover:underline"
-                    type="button"
-                    onClick={() => submit()}
-                    onKeyPress={e => {
+                    variant="primary"
+                    onClick={submit}
+                    onKeyPress={(e: React.KeyboardEvent<HTMLButtonElement>) => {
                         if (e.key === 'Enter') {
                             submit()
                         }
                     }}>
                     Opslaan
-                </button>
+                </Button>
             </div>
         </div>
     )
