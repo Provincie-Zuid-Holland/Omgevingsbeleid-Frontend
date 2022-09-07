7d3ce3ebd1f18b59cbfb7e6413ecc94ba90cfa62
Aiden Buis <hello@aidenbuis.com>
2022-09-07 16:01:05 +0200

    ♻️ Clean up component

--- src/components/Container/ContainerMain/ContainerMain.tsx
+++ src/components/Container/ContainerMain/ContainerMain.tsx
@@ -2,13 +2,6 @@ import classNames from 'classnames'
 import { FC } from 'react'
 
 /**
- * Pages met aparte container:
- * /pages/Login
- * /pages/MuteerUniversalObjectCRUD/ContainerMain
- * /components/Navigation
- * */
-
-/**
  * Displays either a container with or without an id.
  */
 
@@ -18,22 +11,13 @@ interface ContainerProps {
 }
 
 const ContainerMain: FC<ContainerProps> = ({ id, children, className }) => {
-    if (id)
-        return (
-            <div
-                className={classNames(
-                    `container flex pb-8 mx-auto sm:px-6 lg:px-8`,
-                    {
-                        className,
-                    }
-                )}
-                id={id}>
-                {children}
-            </div>
-        )
-
     return (
-        <div className="container flex pb-8 mx-auto sm:px-6 lg:px-8">
+        <div
+            id={id}
+            className={classNames(
+                `container flex pb-8 mx-auto sm:px-6 lg:px-8`,
+                className
+            )}>
             {children}
         </div>
     )
