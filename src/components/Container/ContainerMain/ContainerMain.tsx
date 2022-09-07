a2c8246ee7ec6bbc0b90d537312e52cb44718205
Aiden Buis <hello@aidenbuis.com>
2022-09-07 10:45:02 +0200

    💄 Add className parameter

--- src/components/Container/ContainerMain/ContainerMain.tsx
+++ src/components/Container/ContainerMain/ContainerMain.tsx
@@ -1,3 +1,4 @@
+import classNames from 'classnames'
 import { FC } from 'react'
 
 /**
@@ -13,13 +14,19 @@ import { FC } from 'react'
 
 interface ContainerProps {
     id?: string
+    className?: string
 }
 
-const ContainerMain: FC<ContainerProps> = ({ id, children }) => {
+const ContainerMain: FC<ContainerProps> = ({ id, children, className }) => {
     if (id)
         return (
             <div
-                className="container flex pb-8 mx-auto sm:px-6 lg:px-8"
+                className={classNames(
+                    `container flex pb-8 mx-auto sm:px-6 lg:px-8`,
+                    {
+                        className,
+                    }
+                )}
                 id={id}>
                 {children}
             </div>
