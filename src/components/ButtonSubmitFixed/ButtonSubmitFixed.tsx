4deceddb967b87563573e5875c3ef5319e3c5bab
Aiden Buis <hello@aidenbuis.com>
2022-09-15 16:52:51 +0200

    ✨ Add scroll to functionality when invalid click

--- src/components/ButtonSubmitFixed/ButtonSubmitFixed.tsx
+++ src/components/ButtonSubmitFixed/ButtonSubmitFixed.tsx
@@ -1,28 +1,47 @@
 import { Button } from '@pzh-ui/components'
 import { KeyboardEvent, MouseEvent } from 'react'
 
+import scrollToFormikError from '@/utils/scrollToFormikError'
+
 /**
  * Displays a fixed placed submit button.
  */
 interface ButtonSubmitFixedProps {
     submit?: ((e: MouseEvent | KeyboardEvent) => void) | null
+    disabled?: boolean
+    scrollToError?: () => void
 }
 
-function ButtonSubmitFixed({ submit }: ButtonSubmitFixedProps) {
+function ButtonSubmitFixed({
+    submit,
+    disabled,
+    scrollToError,
+}: ButtonSubmitFixedProps) {
     if (!submit) return null
 
     return (
         <div className="fixed bottom-0 right-0 z-10 px-6">
-            <div className="inline-block px-4 py-4 bg-white rounded-t shadow">
+            <div className="relative z-10 inline-block px-4 py-4 bg-white rounded-t shadow">
+                <div
+                    className="absolute top-0 left-0 z-10 w-full h-full"
+                    onClick={() => {
+                        if (disabled && scrollToError) {
+                            scrollToError()
+                        }
+                    }}
+                />
                 <Button
                     id="form-submit"
                     type="button"
-                    onClick={e => submit(e)}
+                    onClick={e => {
+                        submit(e)
+                    }}
                     onKeyPress={e => {
                         if (e.key === 'Enter') {
                             submit(e)
                         }
-                    }}>
+                    }}
+                    disabled={disabled}>
                     Opslaan
                 </Button>
             </div>
