16e1ddd8f179142bd13d79bda2fc2a18a81087f5
Aiden Buis <hello@aidenbuis.com>
2022-09-19 12:01:36 +0200

    🐛 Fix clickthrough on not disabled btn

--- src/components/ButtonSubmitFixed/ButtonSubmitFixed.tsx
+++ src/components/ButtonSubmitFixed/ButtonSubmitFixed.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@pzh-ui/components'
+import classNames from 'classnames'
 import { KeyboardEvent, MouseEvent } from 'react'
 
 /**
@@ -21,7 +22,10 @@ function ButtonSubmitFixed({
         <div className="fixed bottom-0 right-0 z-10 px-6">
             <div className="relative z-10 inline-block px-4 py-4 bg-white rounded-t shadow">
                 <div
-                    className="absolute top-0 left-0 z-10 w-full h-full"
+                    className={classNames(
+                        'absolute top-0 left-0 z-10 w-full h-full',
+                        disabled ? '' : 'pointer-events-none'
+                    )}
                     onClick={() => {
                         if (disabled && scrollToError) {
                             scrollToError()
