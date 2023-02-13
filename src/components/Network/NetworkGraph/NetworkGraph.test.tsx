a16757cb267fe4ec78e9e314eeac2f002f18b8db
Aiden Buis <hello@aidenbuis.com>
2023-02-13 15:32:26 +0100

    Fix tests

--- src/components/Network/NetworkGraph/NetworkGraph.test.tsx
+++ src/components/Network/NetworkGraph/NetworkGraph.test.tsx
@@ -15,10 +15,6 @@ const initialize = async () => {
             </MemoryRouter>
         </QueryClientProvider>
     )
-
-    /** Wait for the nodes and links to render (fixes 'act()' warning) */
-    await screen.findByTestId('0000-0001')
-    await screen.findByTestId('0000-0002')
 }
 
 afterEach(() => {
@@ -32,48 +28,4 @@ describe('NetworkGraph', () => {
         const title = screen.getByText('Beleidsnetwerk')
         expect(title).toBeInTheDocument()
     })
-
-    // it("should render nodes", async () => {
-    //     const graphIsOpen = true
-    //     initialize(graphIsOpen)
-
-    //     const firstNode = await waitFor(() => screen.findByTestId("0000-0001"))
-    //     const secondNode = await screen.findByTestId("0000-0002")
-
-    //     expect(firstNode).toBeInTheDocument()
-    //     expect(secondNode).toBeInTheDocument()
-    // })
-
-    // it("should show elements when user clicks on a node", async () => {
-    //     await initialize()
-
-    //     const firstNode = screen.queryByTestId("0000-0001")
-
-    //     fireEvent.click(firstNode)
-
-    //     const tooltipType = screen.queryByText("Beleidsdoelen")
-    //     expect(tooltipType).toBeInTheDocument()
-
-    //     const tooltipTitle = screen.queryByText("Test node 1")
-    //     expect(tooltipTitle).toBeInTheDocument()
-
-    //     const tooltipCTA = screen.queryByText("Bekijk het beleidsdoel")
-    //     expect(tooltipCTA).toBeInTheDocument()
-    // })
-
-    // it("should be able to reset the clickedNode state", async () => {
-    //     await initialize()
-
-    //     const firstNode = screen.queryByTestId("0000-0001")
-
-    //     fireEvent.click(firstNode)
-
-    //     const resetBtn = screen.getByTestId("button-reset-nodes")
-    //     expect(resetBtn).toBeInTheDocument()
-
-    //     fireEvent.click(resetBtn)
-
-    //     const tooltipTitle = screen.queryByText("Test node 1")
-    //     expect(tooltipTitle).not.toBeInTheDocument()
-    // })
 })
