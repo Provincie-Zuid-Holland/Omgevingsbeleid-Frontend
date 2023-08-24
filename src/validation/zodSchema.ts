07b20016d96cba72c202ea7bf548069fd4106a0e
Stef Winterswijk <stef_winterswijk@hotmail.com>
2023-08-24 10:47:15 +0200

    update validation

--- src/validation/zodSchema.ts
+++ src/validation/zodSchema.ts
@@ -1,45 +1,40 @@
-import { z } from 'zod'
+import { string, number, instanceof as instanceOf } from 'zod'
 
 export const schemaDefaults = {
     requiredString: (msg = 'Dit veld is verplicht.') =>
-        z.string({
+        string({
             required_error: msg,
             invalid_type_error: msg,
         }),
-    optionalString: z.string().optional().nullable(),
+    optionalString: string().optional().nullable(),
     requiredNumber: (msg = 'Dit veld is verplicht.') =>
-        z.number({
+        number({
             required_error: msg,
             invalid_type_error: msg,
         }),
     email: (msg = 'Dit veld is verplicht.') =>
-        z
-            .string({
-                required_error: msg,
-                invalid_type_error: msg,
-            })
-            .email('Onjuist e-mailadres'),
+        string({
+            required_error: msg,
+            invalid_type_error: msg,
+        }).email('Onjuist e-mailadres'),
     url: (msg = 'Dit veld is verplicht.') =>
-        z
-            .string({
-                required_error: msg,
-                invalid_type_error: msg,
-            })
-            .url('Onjuiste link'),
-    optionalUrl: z.string().url('Onjuiste link').optional().nullable(),
-    title: z
-        .string({
-            required_error: 'Vul een titel in',
-            invalid_type_error: 'Vul een titel in',
-        })
+        string({
+            required_error: msg,
+            invalid_type_error: msg,
+        }).url('Onjuiste link'),
+    optionalUrl: string().url('Onjuiste link').optional().nullable(),
+    title: string({
+        required_error: 'Vul een titel in',
+        invalid_type_error: 'Vul een titel in',
+    })
         .min(4, 'Vul een titel in van minimaal 4 karakters')
         .max(100, 'Vul een titel in van maximaal 100 karakters'),
-    date: z
-        .string({ required_error: 'Selecteer een datum' })
-        .datetime('Onjuiste datum'),
-    file: z.instanceof(File),
+    date: string({ required_error: 'Selecteer een datum' }).datetime(
+        'Onjuiste datum'
+    ),
+    file: instanceOf(File),
 }
 
 export type Validation = {
-    [K in keyof typeof schemaDefaults]?: typeof schemaDefaults[K]
+    [K in keyof typeof schemaDefaults]?: (typeof schemaDefaults)[K]
 }
