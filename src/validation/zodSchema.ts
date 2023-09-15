4da8d0def445ddab75377dec62f8bfdc062a532c
Stef Winterswijk <stef_winterswijk@hotmail.com>
2023-09-15 14:07:56 +0200

    AB#15229 Update email validation

--- src/validation/zodSchema.ts
+++ src/validation/zodSchema.ts
@@ -1,4 +1,4 @@
-import { string, number, instanceof as instanceOf } from 'zod'
+import { instanceof as instanceOf, number, string } from 'zod'
 
 export const schemaDefaults = {
     requiredString: (msg = 'Dit veld is verplicht.') =>
@@ -16,7 +16,9 @@ export const schemaDefaults = {
         string({
             required_error: msg,
             invalid_type_error: msg,
-        }).email('Onjuist e-mailadres'),
+        })
+            .email('Onjuist e-mailadres')
+            .max(255, 'Vul een e-mailadres in van maximaal 255 karakters'),
     url: (msg = 'Dit veld is verplicht.') =>
         string({
             required_error: msg,
@@ -29,8 +31,9 @@ export const schemaDefaults = {
     })
         .min(4, 'Vul een titel in van minimaal 4 karakters')
         .max(220, 'Vul een titel in van maximaal 220 karakters'),
-    date: string({ required_error: 'Selecteer een datum' })
-        .datetime('Onjuiste datum'),
+    date: string({ required_error: 'Selecteer een datum' }).datetime(
+        'Onjuiste datum'
+    ),
     file: instanceOf(File),
 }
 
