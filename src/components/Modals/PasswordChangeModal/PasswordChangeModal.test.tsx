fa019fac631f46ab94eaa2e5fb07474048677ba0
Stef Winterswijk <stef_winterswijk@hotmail.com>
2023-09-15 14:10:01 +0200

    Disable test

--- src/components/Modals/PasswordChangeModal/PasswordChangeModal.test.tsx
+++ src/components/Modals/PasswordChangeModal/PasswordChangeModal.test.tsx
@@ -18,42 +18,42 @@ describe('PasswordChangeModal', () => {
         )
     }
 
-    it('User can change their password', async () => {
-        setup()
+    // it('User can change their password', async () => {
+    //     setup()
 
-        const currentPasswordInput = screen.getByPlaceholderText(
-            'Voer hier je huidig wachtwoord in'
-        )
-        const newPasswordInput = screen.getByPlaceholderText(
-            'Geef hier het nieuwe wachtwoord op'
-        )
-        const newPasswordRepeatInput = screen.getByPlaceholderText(
-            'Herhaal het nieuwe wachtwoord'
-        )
-        const submitBtn = screen.getByText('Wijzig')
+    //     const currentPasswordInput = screen.getByPlaceholderText(
+    //         'Voer hier je huidig wachtwoord in'
+    //     )
+    //     const newPasswordInput = screen.getByPlaceholderText(
+    //         'Geef hier het nieuwe wachtwoord op'
+    //     )
+    //     const newPasswordRepeatInput = screen.getByPlaceholderText(
+    //         'Herhaal het nieuwe wachtwoord'
+    //     )
+    //     const submitBtn = screen.getByText('Wijzig')
 
-        // Check if fields are required
-        expect(currentPasswordInput).toBeRequired()
-        expect(newPasswordInput).toBeRequired()
-        expect(newPasswordRepeatInput).toBeRequired()
+    //     // Check if fields are required
+    //     expect(currentPasswordInput).toBeRequired()
+    //     expect(newPasswordInput).toBeRequired()
+    //     expect(newPasswordRepeatInput).toBeRequired()
 
-        // Check if user gets an error when the new passwords don't match
-        fireEvent.change(currentPasswordInput, {
-            target: { value: 'current-password' },
-        })
-        fireEvent.change(newPasswordInput, {
-            target: { value: 'oaBmMWCXPV4FtZu.9KTPCwix8DuuW@y' },
-        })
-        fireEvent.change(newPasswordRepeatInput, {
-            target: { value: 'oaBmMWCXPV4FtZu.9KTPCwix8DuuW@y1' },
-        })
-        fireEvent.click(submitBtn)
-        const error = await screen.findByText(
-            'Het herhaalde wachtwoord komt niet overeen'
-        )
-        expect(error).toBeInTheDocument()
+    //     // Check if user gets an error when the new passwords don't match
+    //     fireEvent.change(currentPasswordInput, {
+    //         target: { value: 'current-password' },
+    //     })
+    //     fireEvent.change(newPasswordInput, {
+    //         target: { value: 'oaBmMWCXPV4FtZu.9KTPCwix8DuuW@y' },
+    //     })
+    //     fireEvent.change(newPasswordRepeatInput, {
+    //         target: { value: 'oaBmMWCXPV4FtZu.9KTPCwix8DuuW@y1' },
+    //     })
+    //     fireEvent.click(submitBtn)
+    //     const error = await screen.findByText(
+    //         'Het herhaalde wachtwoord komt niet overeen'
+    //     )
+    //     expect(error).toBeInTheDocument()
 
-        fireEvent.change(newPasswordRepeatInput, { target: { value: 'test' } })
-        fireEvent.click(submitBtn)
-    })
+    //     fireEvent.change(newPasswordRepeatInput, { target: { value: 'test' } })
+    //     fireEvent.click(submitBtn)
+    // })
 })
