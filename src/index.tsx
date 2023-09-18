dad2c89f972eba95d19d06c4d2be4f1f9437fc4a
Stef Winterswijk <stef_winterswijk@hotmail.com>
2023-09-18 15:22:53 +0200

    Remove unnecessary polyfills

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
 import ScrollToTop from './components/ScrollToTop'
-import { polyfills } from './utils/polyfills.js'
-
-polyfills()
 
 const container = document.getElementById('root')
 
