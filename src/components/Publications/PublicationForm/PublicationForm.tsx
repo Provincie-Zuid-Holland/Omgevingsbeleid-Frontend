2053b6937499f8d417faee260efae606df2d6a95
Stef Winterswijk <stef_winterswijk@hotmail.com>
2024-02-27 14:19:21 +0100

    Add Form validation

--- src/components/Publications/PublicationForm/PublicationForm.tsx
+++ src/components/Publications/PublicationForm/PublicationForm.tsx
@@ -1,8 +1,10 @@
 import { Button, Divider, FormikInput, FormikSelect } from '@pzh-ui/components'
 import { Form, Formik, FormikConfig, FormikValues } from 'formik'
+import { toFormikValidationSchema } from 'zod-formik-adapter'
 
 import { AppExtensionsPublicationsEnumsDocumentType } from '@/api/fetchers.schemas'
 import useModalStore from '@/store/modalStore'
+import { PUBLICATION_SCHEMA } from '@/validation/publication'
 
 interface PublicationFormProps {
     submitLabel: string
@@ -21,30 +23,44 @@ const PublicationForm = <TData extends FormikValues>({
     ).map(type => ({ label: type, value: type }))
 
     return (
-        <Formik enableReinitialize {...rest}>
+        <Formik
+            enableReinitialize
+            validationSchema={toFormikValidationSchema(PUBLICATION_SCHEMA)}
+            {...rest}>
             {({ isSubmitting }) => (
                 <Form>
                     <div className="space-y-4">
-                        <FormikSelect
-                            name="Document_Type"
-                            label="Instrument"
-                            options={options}
-                            disabled
-                        />
-                        <FormikInput
-                            name="Official_Title"
-                            label="Officiële titel"
-                            placeholder="Vul de officiële titel in"
-                        />
-                        <FormikInput
-                            name="Regulation_Title"
-                            label="Regeling opschrift"
-                            placeholder="Regeling opschrift"
-                        />
-                        <FormikSelect
-                            name="Template_ID"
-                            label="Publicatie template"
-                        />
+                        <div>
+                            <FormikSelect
+                                name="Document_Type"
+                                label="Instrument"
+                                options={options}
+                                disabled
+                                required
+                            />
+                        </div>
+                        <div>
+                            <FormikInput
+                                name="Official_Title"
+                                label="Officiële titel"
+                                placeholder="Vul de officiële titel in"
+                                required
+                            />
+                        </div>
+                        <div>
+                            <FormikInput
+                                name="Regulation_Title"
+                                label="Regeling opschrift"
+                                placeholder="Regeling opschrift"
+                                required
+                            />
+                        </div>
+                        <div>
+                            <FormikSelect
+                                name="Template_ID"
+                                label="Publicatie template"
+                            />
+                        </div>
                     </div>
                     <Divider className="my-6" />
                     <div className="flex items-center justify-between">
