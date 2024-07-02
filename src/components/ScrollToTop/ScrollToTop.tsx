c971d819b638c16e0fc28988d2ad809f92c76e12
Stef Winterswijk <stef_winterswijk@hotmail.com>
2024-07-02 15:11:24 +0200

    AB#19714

--- src/components/ScrollToTop/ScrollToTop.tsx
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -5,7 +5,7 @@ import { useLocation } from 'react-router-dom'
  * Function that can be called to bring the user's screen to the top of the page.
  */
 export default function ScrollToTop() {
-    const { pathname } = useLocation()
+    const { pathname, key } = useLocation()
 
     useEffect(() => {
         window.scrollTo({
@@ -13,7 +13,7 @@ export default function ScrollToTop() {
             top: 0,
             behavior: 'instant',
         })
-    }, [pathname])
+    }, [pathname, key])
 
     return null
 }
