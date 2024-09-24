d47fc0cdb41f159b1c90c13e50a7315b5ad744ac
Stef Winterswijk <stef_winterswijk@hotmail.com>
2024-09-24 15:23:57 +0200

    Remove old publication files

--- src/components/Modals/PublicationModals/PublicationPackagesModal/steps/types.ts
+++
@@ -1,3 +0,0 @@
-export interface StepProps {
-    setStep: (step: number) => void
-}
--- src/components/Modals/PublicationModals/index.ts
+++ src/components/Modals/PublicationModals/index.ts
@@ -1,17 +1,9 @@
-import PublicationAddModal from './PublicationAddModal'
 import PublicationAnnouncementUpdateModal from './PublicationAnnouncementUpdateModal'
 import PublicationEditModal from './PublicationEditModal'
-import PublicationPackagesModal from './PublicationPackagesModal'
-import PublicationVersionAbortModal from './PublicationVersionAbortModal'
-import PublicationVersionAddModal from './PublicationVersionAddModal'
 import PublicationVersionEditModal from './PublicationVersionEditModal'
 
 export {
-    PublicationAddModal,
     PublicationAnnouncementUpdateModal,
     PublicationEditModal,
-    PublicationPackagesModal,
-    PublicationVersionAbortModal,
-    PublicationVersionAddModal,
     PublicationVersionEditModal,
 }
