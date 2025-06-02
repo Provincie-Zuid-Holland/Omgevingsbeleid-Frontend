e63a7d61a08ba34e505dc6708a038ee7be8276ed
Stef Winterswijk <stef_winterswijk@hotmail.com>
2025-06-02 14:45:19 +0200

    AB#26545

--- src/components/Indicator/Indicator.tsx
+++ src/components/Indicator/Indicator.tsx
@@ -10,13 +10,13 @@ const Indicator = ({ amount = 0, hasNotification }: IndicatorProps) => (
         className={cn(
             'relative flex h-6 w-6 items-center justify-center rounded-full border',
             {
-                'after:content-[" "] border-pzh-blue-500 bg-pzh-blue-500 text-pzh-white after:absolute after:-left-1 after:-top-1 after:h-3 after:w-3 after:rounded-full after:border after:border-pzh-white after:bg-pzh-red-500':
+                'after:content-[" "] border-pzh-blue-500 bg-pzh-blue-500 text-pzh-white after:border-pzh-white after:bg-pzh-red-500 after:absolute after:-top-1 after:-left-1 after:h-3 after:w-3 after:rounded-full after:border':
                     hasNotification,
                 'border-pzh-blue-100 bg-pzh-blue-10 text-pzh-blue-500':
                     !hasNotification,
             }
         )}>
-        <span className="-mb-1 text-s font-bold">{amount}</span>
+        <span className="text-s -mb-0.5 font-bold">{amount}</span>
     </div>
 )
 
