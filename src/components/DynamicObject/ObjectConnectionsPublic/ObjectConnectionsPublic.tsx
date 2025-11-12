4af732553937a95ace9c2ca74b41ce8e2f91f762
Stef Winterswijk <stef_winterswijk@hotmail.com>
2025-11-12 10:22:27 +0100

    AB19919

--- src/components/DynamicObject/ObjectConnectionsPublic/ObjectConnectionsPublic.tsx
+++ src/components/DynamicObject/ObjectConnectionsPublic/ObjectConnectionsPublic.tsx
@@ -2,7 +2,7 @@ import { Heading, ListLink, Text } from '@pzh-ui/components'
 import { Link } from 'react-router-dom'
 
 import * as models from '@/config/objects'
-import { Model, ModelReturnType } from '@/config/objects/types'
+import { Model, ModelReturnType, ModelType } from '@/config/objects/types'
 import { generateObjectPath } from '@/utils/dynamicObject'
 
 interface ObjectConnectionsPublicProps {
@@ -13,52 +13,86 @@ interface ObjectConnectionsPublicProps {
 const ObjectConnectionsPublic = ({
     model,
     data,
-}: ObjectConnectionsPublicProps) => (
-    <div data-section="Koppelingen">
-        <Heading level="2" className="mb-4">
-            Koppelingen
-        </Heading>
-        <Text className="first-letter:capitalize">
-            {model.connectionsDescription}
-        </Text>
+}: ObjectConnectionsPublicProps) => {
+    const acknowledgedRelationModel =
+        data.Hierarchy_Statics &&
+        models[data.Hierarchy_Statics.Object_Type as ModelType]
 
-        {model.allowedConnections?.map(connection => {
-            const items = data[connection.key]
-            const model = models[connection.type]
+    return (
+        <div data-section="Koppelingen">
+            <Heading level="2" className="mb-4">
+                Koppelingen
+            </Heading>
+            <Text className="first-letter:capitalize">
+                {model.connectionsDescription}
+            </Text>
 
-            return (
-                <div key={connection.type} className="mt-6">
+            {acknowledgedRelationModel && (
+                <div className="mt-6">
                     <Heading level="3" size="m" className="mb-2">
-                        {model.defaults.pluralCapitalize}
+                        {acknowledgedRelationModel.defaults.pluralCapitalize}
                     </Heading>
 
-                    {Array.isArray(items) && items.length > 0 ? (
-                        <ul>
-                            {(items as any[]).map(item => (
-                                <li key={item.Object.UUID}>
-                                    <ListLink
-                                        asChild
-                                        className="text-pzh-green-500 hover:text-pzh-green-900">
-                                        <Link
-                                            to={generateObjectPath(
-                                                connection.type,
-                                                item.Object.UUID
-                                            )}>
-                                            {item.Object.Title}
-                                        </Link>
-                                    </ListLink>
-                                </li>
-                            ))}
-                        </ul>
-                    ) : (
-                        <span className="italic text-pzh-gray-600">
-                            Er zijn nog geen {model.defaults.plural} gekoppeld
-                        </span>
-                    )}
+                    <ul>
+                        <li>
+                            <ListLink
+                                asChild
+                                className="text-pzh-green-500 hover:text-pzh-green-900">
+                                <Link
+                                    to={generateObjectPath(
+                                        acknowledgedRelationModel.defaults
+                                            .singular,
+                                        String(
+                                            data.Hierarchy_Statics?.Object_ID
+                                        )
+                                    )}>
+                                    {data.Hierarchy_Statics?.Cached_Title}
+                                </Link>
+                            </ListLink>
+                        </li>
+                    </ul>
                 </div>
-            )
-        })}
-    </div>
-)
+            )}
+
+            {model.allowedConnections?.map(connection => {
+                const items = data[connection.key]
+                const model = models[connection.type]
+
+                return (
+                    <div key={connection.type} className="mt-6">
+                        <Heading level="3" size="m" className="mb-2">
+                            {model.defaults.pluralCapitalize}
+                        </Heading>
+
+                        {Array.isArray(items) && items.length > 0 ? (
+                            <ul>
+                                {(items as any[]).map(item => (
+                                    <li key={item.Object.UUID}>
+                                        <ListLink
+                                            asChild
+                                            className="text-pzh-green-500 hover:text-pzh-green-900">
+                                            <Link
+                                                to={generateObjectPath(
+                                                    connection.type,
+                                                    item.Object.UUID
+                                                )}>
+                                                {item.Object.Title}
+                                            </Link>
+                                        </ListLink>
+                                    </li>
+                                ))}
+                            </ul>
+                        ) : (
+                            <span className="text-pzh-gray-600 italic">
+                                Er zijn nog geen {model.defaults.plural}{' '}
+                                gekoppeld
+                            </span>
+                        )}
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
 
 export default ObjectConnectionsPublic
