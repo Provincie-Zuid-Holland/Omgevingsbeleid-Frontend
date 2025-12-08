0cd753d5a1c1a2077a7b19a3290249e827f038ad
Stef Winterswijk <stef_winterswijk@hotmail.com>
2025-12-08 13:06:51 +0100

    AB#19919

--- src/components/DynamicObject/ObjectConnectionsPublic/ObjectConnectionsPublic.tsx
+++ src/components/DynamicObject/ObjectConnectionsPublic/ObjectConnectionsPublic.tsx
@@ -1,9 +1,12 @@
 import { Heading, ListLink, Text } from '@pzh-ui/components'
 import { Link } from 'react-router-dom'
 
+import { HierachyReference } from '@/api/fetchers.schemas'
 import * as models from '@/config/objects'
 import { Model, ModelReturnType, ModelType } from '@/config/objects/types'
 import { generateObjectPath } from '@/utils/dynamicObject'
+import groupBy from 'lodash.groupby'
+import { useMemo } from 'react'
 
 interface ObjectConnectionsPublicProps {
     model: Model
@@ -17,6 +20,20 @@ const ObjectConnectionsPublic = ({
     const acknowledgedRelationModel =
         data.Hierarchy_Statics &&
         models[data.Hierarchy_Statics.Object_Type as ModelType]
+
+    const relationChildrens = useMemo(() => {
+        if (!data.Hierarchy_Children) return
+
+        const grouped = groupBy(data.Hierarchy_Children, 'Object_Type')
+        const sorted = Object.keys(grouped)
+            .sort()
+            .reduce((obj: any, key) => {
+                obj[key] = grouped[key]
+                return obj
+            }, {})
+
+        return sorted
+    }, [data.Hierarchy_Children]) as { [key in ModelType]: HierachyReference[] }
 
     return (
         <div data-section="Koppelingen">
@@ -54,43 +71,47 @@ const ObjectConnectionsPublic = ({
                 </div>
             )}
 
-            {model.allowedConnections?.map(connection => {
-                const items = data[connection.key]
-                const model = models[connection.type]
+            {!!relationChildrens &&
+                Object.keys(relationChildrens).map(type => {
+                    const model = models[type as ModelType]
+                    const items =
+                        relationChildrens[
+                            type as keyof typeof relationChildrens
+                        ]
 
-                return (
-                    <div key={connection.type} className="mt-6">
-                        <Heading level="3" size="m" className="mb-2">
-                            {model.defaults.pluralCapitalize}
-                        </Heading>
+                    return (
+                        <div key={type} className="mt-6">
+                            <Heading level="3" size="m" className="mb-2">
+                                {model.defaults.pluralCapitalize}
+                            </Heading>
 
-                        {Array.isArray(items) && items.length > 0 ? (
-                            <ul>
-                                {(items as any[]).map(item => (
-                                    <li key={item.Object.UUID}>
-                                        <ListLink
-                                            asChild
-                                            className="text-pzh-green-500 hover:text-pzh-green-900">
-                                            <Link
-                                                to={generateObjectPath(
-                                                    connection.type,
-                                                    item.Object.UUID
-                                                )}>
-                                                {item.Object.Title}
-                                            </Link>
-                                        </ListLink>
-                                    </li>
-                                ))}
-                            </ul>
-                        ) : (
-                            <span className="text-pzh-gray-600 italic">
-                                Er zijn nog geen {model.defaults.plural}{' '}
-                                gekoppeld
-                            </span>
-                        )}
-                    </div>
-                )
-            })}
+                            {Array.isArray(items) && items.length > 0 ? (
+                                <ul>
+                                    {(items as any[]).map(item => (
+                                        <li key={item.UUID}>
+                                            <ListLink
+                                                asChild
+                                                className="text-pzh-green-500 hover:text-pzh-green-900">
+                                                <Link
+                                                    to={generateObjectPath(
+                                                        type as ModelType,
+                                                        item.UUID
+                                                    )}>
+                                                    {item.Title}
+                                                </Link>
+                                            </ListLink>
+                                        </li>
+                                    ))}
+                                </ul>
+                            ) : (
+                                <span className="text-pzh-gray-600 italic">
+                                    Er zijn nog geen {model.defaults.plural}{' '}
+                                    gekoppeld
+                                </span>
+                            )}
+                        </div>
+                    )
+                })}
         </div>
     )
 }
