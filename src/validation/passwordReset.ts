da3698142b9171965bad25208cfc0fcc079c1bde
Stef Winterswijk <stef_winterswijk@hotmail.com>
2026-04-21 13:12:58 +0200

    AB#34200

--- src/validation/passwordReset.ts
+++ src/validation/passwordReset.ts
@@ -1,14 +1,23 @@
 import { ZodIssueCode, object, string } from 'zod'
 
+const noLeadingOrTrailingSpaces = (value: string) => value === value.trim()
+
 export const SCHEMA = object({
     currentPassword: string({
         required_error: 'Vul je huidige wachtwoord in',
-    }),
+    }).refine(
+        noLeadingOrTrailingSpaces,
+        'Spaties aan het begin of einde zijn niet toegestaan'
+    ),
     newPassword: string({ required_error: 'Vul een nieuw wachtwoord in' })
         .min(12, 'Gebruik minimaal 12 tekens')
         .regex(
             /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{12,}$/,
             'Gebruik minimaal 1 hoofdletter, 1 kleine letter, 1 cijfer en 1 speciaal teken'
+        )
+        .refine(
+            noLeadingOrTrailingSpaces,
+            'Spaties aan het begin of einde zijn niet toegestaan'
         ),
     newPasswordCopy: string({
         required_error: 'Herhaal hier je nieuwe wachtwoord',
